d56ca57f81aa2c6df171b8f43f388547e4aac292
zhaolifeng <zhaolifeng81@163.com>
2020-10-09 11:29:36 +0800

    navbar

---
+++ pages/component/navbar/navbar.js
@@ -0,0 +1,46 @@
+// pages/component/navbar/navbar.js
+Component({
+  properties: {
+    /**
+     * 自定义返回事件处理
+     * customBackReturn="{{true}}" bind:customBackReturn="customBackReturn"
+     */
+    customBackReturn: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    backClick() {
+      console.log("$$$$$$$$$$$$$$")
+      // if (this.data.customBackReturn) {
+      //   this.triggerEvent("customBackReturn")
+      // } else {
+      //   if (getCurrentPages().length == 1) {
+      //     wx.switchTab({
+      //       url: '/pages/index/index',
+      //     })
+      //   } else {
+      //     wx.navigateBack({
+      //       delta: 1
+      //     })
+      //   } 
+      // }
+    }
+  },
+  attached() {
+    var self = this;
+    wx.getSystemInfo({
+      success(res) {
+        var isIos = res.system.indexOf('iOS') > -1;
+        self.setData({
+          statusHeight: res.statusBarHeight,
+          navHeight: isIos ? 44 : 48
+        })
+      }
+    })
+  }
+})
